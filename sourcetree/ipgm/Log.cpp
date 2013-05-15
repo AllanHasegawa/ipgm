4210ef3c5e35a0cb6f08c3a8fac2bbeb0e028fe8
Allan Hasegawa <sirhase@gmail.com>
2013-05-15 02:21:07 -0300

    Log class now supports 3 levels of logging

--- sourcetree/ipgm/Log.cpp
+++ sourcetree/ipgm/Log.cpp
@@ -1,2 +1,4 @@
 
 #include "Log.hpp"
+
+ipgm::LOG_LEVEL ipgm::Log::log_level_ = ipgm::LOG_LEVEL::DEBUG;
--- sourcetree/ipgm/include/Log.hpp
+++ sourcetree/ipgm/include/Log.hpp
@@ -5,12 +5,19 @@
 #include "windows.h"
 
 namespace ipgm {
+	enum class LOG_LEVEL {
+		RELEASE,
+		INFO,
+		DEBUG
+	};
 
 	class Log {
 	private:
 		struct _LCK {};
 
 	public:
+		static LOG_LEVEL log_level_;
+
 		Log(const _LCK L) {
 			AllocConsole();
 			freopen( "CONOUT$", "wb", stdout);
@@ -21,7 +28,35 @@ namespace ipgm {
 			FreeConsole();
 		}
 
-		void printf(const char *str,...) {
+		void dprintf(const char *str,...) {
+			if (log_level_ == LOG_LEVEL::DEBUG) {
+				va_list al;
+				va_start(al,str);
+				vprintf(str, al);
+				va_end(al);
+			}
+		}
+
+		void iprintf(const char *str,...) {
+			if (log_level_ == LOG_LEVEL::INFO || log_level_ == LOG_LEVEL::DEBUG) {
+				va_list al;
+				va_start(al,str);
+				vprintf(str, al);
+				va_end(al);
+			}
+		}
+
+		void rprintf(const char *str,...) {
+			va_list al;
+			va_start(al,str);
+			vprintf(str, al);
+			va_end(al);
+		}
+
+		void printf(const LOG_LEVEL ll, const char *str,...) {
+			if (ll == LOG_LEVEL::DEBUG && log_level_ == LOG_LEVEL::RELEASE) {
+				return;
+			}
 			va_list al;
 			va_start(al,str);
 			vprintf(str, al);
